d151b464b9dbaf00af38c33ff2e9cc458a32c256
jainilsoni1706 <jainilsoni1706@gmail.com>
2023-01-30 23:17:50 +0530

    show hide pending

--- jainil.js
+++ jainil.js
@@ -6,8 +6,61 @@ class jLib {
         class: ".",
         id: '#',
         data: '*',
-        placeholder: '&'
+        placeholder: '&',
+        tag: '>',
     };
+
+    static htmlElements = {
+        div: 'div',
+        span: 'span',
+        p: 'p',
+        h1: 'h1',
+        h2: 'h2',
+        h3: 'h3',
+        h4: 'h4',
+        h5: 'h5',
+        h6: 'h6',
+        a: 'a',
+        img: 'img',
+        button: 'button',
+        input: 'input',
+        textarea: 'textarea',
+        select: 'select',
+        option: 'option',
+        form: 'form',
+        label: 'label',
+        table: 'table',
+        tr: 'tr',
+        td: 'td',
+        th: 'th',
+        thead: 'thead',
+        tbody: 'tbody',
+        tfoot: 'tfoot',
+        ul: 'ul',
+        ol: 'ol',
+        li: 'li',
+        script: 'script',
+        link: 'link',
+        meta: 'meta',
+        style: 'style',
+        header: 'header',
+        footer: 'footer',
+        main: 'main',
+        section: 'section',
+        article: 'article',
+        aside: 'aside',
+        nav: 'nav',
+        video: 'video',
+        audio: 'audio',
+        source: 'source',
+        canvas: 'canvas',
+        svg: 'svg',
+        iframe: 'iframe',
+        embed: 'embed',
+        object: 'object',
+    }
+
+    static selectedElement = null;
 
     constructor(selectors = {}) {
 
@@ -21,26 +74,33 @@ class jLib {
         if (typeof element == "string") {
             let selectorOfArgument = element[0];
             if (Object.values(this.selectors).indexOf(selectorOfArgument) > -1) {
-                return Object.keys(this.selectors)[Object.values(this.selectors).indexOf(selectorOfArgument)]
+                return Object.keys(this.selectors)[Object.values(this.selectors).indexOf(selectorOfArgument)];
             } else {
-                this.error('Invalid selector passed');                
+                this.error('jLib : Invalid selector');
             }
         } else {
-            this.error('Invalid argument passed');
+            this.error('jLib : Invalid argument passed');
         }
     }
 
-    static onClick(element, callable) {
-        let z = this.eventPaser(this.replaceTheseFromString(this.selectors,"",element), this.elementSelectorParser(element),callable, 'click');
+    static on(eventName, element, callable) {
+        this.eventPaser(this.replaceTheseFromString(this.selectors,"",element), this.elementSelectorParser(element),callable, eventName);
     }
 
     static eventPaser(element, selector, callable, event) {
         if (selector == 'class') {
-            document.querySelector(`.${element}`).addEventListener(event, callable);
+            const allClasses = document.querySelectorAll(`.${element}`);
+            for (let i = 0; i < allClasses.length; i++) {
+                allClasses[i].addEventListener(event, callable);
+            }
         } else if (selector == "id") {
             return document.getElementById(element).addEventListener(event, callable);
+        } else if (selector == "tag") {
+            const thisTag = document.querySelectorAll(`${element}`);
+            for (let i = 0; i < thisTag.length; i++) {
+                thisTag[i].addEventListener(event, callable);
+            }
         }
- 
     }
 
     static replaceTheseFromString(oldstr, newstr, element)
@@ -56,6 +116,23 @@ class jLib {
         }
 
         return element;
+    }
+
+    static select(element) {
+        this.selectedElement = this.replaceTheseFromString(this.selectors, "", element);
+        return this;
+    }
+
+    static show() {
+        this.eventPaser(this.selectedElement, this.elementSelectorParser(this.selectedElement), (e) => {
+            e.target.style.display = "block";
+        }, "click");        
+    }
+
+    static hide() {
+        this.eventPaser(this.selectedElement, this.elementSelectorParser(this.selectedElement), (e) => {
+            e.target.style.display = "none";
+        }, "click");
     }
 
     static log(...args) {

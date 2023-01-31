4d80004a10bcce172e9e0fceba7337a3975e0a4f
jainilsoni1706 <jainilsoni1706@gmail.com>
2023-01-31 23:13:19 +0530

    hide,show and style method

--- jainil.js
+++ jainil.js
@@ -62,16 +62,26 @@ class jLib {
 
     static selectedElement = null;
 
+    static dateCodes = {
+        d: 'getDate',
+        m: 'getMonth',
+        y: 'getFullYear',
+        h: 'getHours',
+        i: 'getMinutes',
+        s: 'getSeconds',
+        w: 'getDay',
+    };
+
     constructor(selectors = {}) {
 
-        if(typeof selectors == 'object' && JSON.stringify(selectors) !== "{}") {
+        if(this.isObj(selectors) && !this.isArr(selectors) && !this.isEmptyObject(selectors)) {
             this.selectors = selectors;
         }
 
     }
 
     static elementSelectorParser(element) {
-        if (typeof element == "string") {
+        if (this.isStr(element)) {
             let selectorOfArgument = element[0];
             if (Object.values(this.selectors).indexOf(selectorOfArgument) > -1) {
                 return Object.keys(this.selectors)[Object.values(this.selectors).indexOf(selectorOfArgument)];
@@ -119,21 +129,136 @@ class jLib {
     }
 
     static select(element) {
-        this.selectedElement = this.replaceTheseFromString(this.selectors, "", element);
+        this.selectedElement = {
+            'elementType' : this.elementSelectorParser(element),
+            'elementName' : this.replaceTheseFromString(this.selectors,"",element)
+        };
+
+        if (this.selectedElement.elementType == 'class') {            
+            Object.assign( this.selectedElement, {'element' : document.querySelectorAll(`.${this.selectedElement.elementName}`)} );
+        } else if (this.selectedElement.elementType == "id") {
+            Object.assign( this.selectedElement, {'element' : document.querySelector(`#${this.selectedElement.elementName}`)} );
+        } else if (this.selectedElement.elementType == "tag") {
+            Object.assign( this.selectedElement, {'element' : document.querySelectorAll(`${this.selectedElement.elementName}`)} );
+        }        
+
         return this;
     }
 
     static show() {
-        this.eventPaser(this.selectedElement, this.elementSelectorParser(this.selectedElement), (e) => {
-            e.target.style.display = "block";
-        }, "click");        
+        for (let i = 0; i < this.selectedElement.element.length; i++) {
+            this.selectedElement.element[i].style.display = "block";
+        }
     }
 
     static hide() {
-        this.eventPaser(this.selectedElement, this.elementSelectorParser(this.selectedElement), (e) => {
-            e.target.style.display = "none";
-        }, "click");
+        for (let i = 0; i < this.selectedElement.element.length; i++) {
+            this.selectedElement.element[i].style.display = "none";
+        }
     }
+
+    static style(styleObject) {
+        if (this.isObj(styleObject) && !this.isArr(styleObject)) {
+            if (!this.isEmptyObject(styleObject)) {
+                for (let style in styleObject) {
+                    for (let i = 0; i < this.selectedElement.element.length; i++) {
+                        this.selectedElement.element[i].style[style] = styleObject[style];
+                    }
+                }
+            } 
+        } else {
+            if (this.isNum(styleObject)) {
+                return window.getComputedStyle(this.selectedElement.element[styleObject]);
+            } else if (this.isStr(styleObject)) {
+                let styles = {};
+                for (let i = 0; i < this.selectedElement.element.length; i++) { 
+                    styles[i] = window.getComputedStyle(this.selectedElement.element[i]).getPropertyValue(styleObject);
+                }
+                return styles;
+            } else if (this.isArr(styleObject)) {
+                return window.getComputedStyle(this.selectedElement.element[styleObject[0]]).getPropertyValue(styleObject[1]);
+            } else {
+                for (let i = 0; i < this.selectedElement.element.length; i++) {
+                    return window.getComputedStyle(this.selectedElement.element[i]);
+                }
+            }
+        }
+    }
+
+
+    // helper methods
+
+    static date(argument) {
+        if (!this.isUndefined(argument)) {
+
+            let dateLetters = {};
+
+            [...argument].forEach(element => {
+                if (Object.values(this.dateCodes).indexOf(element) > -1) {
+                    // work in progress
+                }
+            });
+
+
+        } else {
+            return new Date();
+        }
+    }
+
+
+    static isObj(variable) {
+        return typeof variable == 'object';
+    }
+
+    static isStr(variable) {
+        return typeof variable == 'string';
+    }
+
+    static isNum(variable) {
+        return typeof variable == 'number';
+    }
+
+    static isBool(variable) {
+        return typeof variable == 'boolean';
+    }
+
+    static isArr(variable) {
+        return Array.isArray(variable);
+    }
+
+    static isFunc(variable) {
+        return typeof variable == 'function';
+    }
+
+    static isNull(variable) {
+        return variable === null;
+    }
+
+    static isNotNull(variable) {
+        return variable !== null;
+    }
+
+    static isEmpty(variable) {
+        return variable === "";
+    }
+
+    static isEmptyObject(variable) {
+        if (this.isObj(variable)) {
+            return JSON.stringify(variable) === "{}";
+        }
+    }
+
+    static isUndefined(variable) {
+        return typeof variable == 'undefined';
+    }
+
+    static isNan(variable) {
+        return isNaN(variable);
+    }
+
+
+
+    // debugging methods
 
     static log(...args) {
         console.log(...args);
